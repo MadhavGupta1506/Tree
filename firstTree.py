107cc47617b9f8937cb5648ee3221596c4926b16
MadhavGupta1506 <gmadhav857@gmail.com>
2024-05-09 09:46:25 +0530

    Insert

--- firstTree.py
+++ firstTree.py
@@ -6,3 +6,11 @@ class  Node:
 class Tree:
     def createNode(self,data):
         return Node(data)
+    def insert(self,node,data):
+        if(node==None):
+            return self.createNode(data)
+        if(node<node):
+            return self.insert(node.left,data)
+        else:
+            return self.insert(node.right,data)
+        
